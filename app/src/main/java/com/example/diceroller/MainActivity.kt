94cb8b02621f2f8350b427d79b758b5046f20bc5
beejohnson <reinsaway@gmail.com>
2022-07-03 23:01:39 -0700

    Add Second Dice ImageView / refactor logic / Tested - Works!

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -29,8 +29,11 @@ class MainActivity : AppCompatActivity() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val diceRoll2 = dice.roll()
+
 
         // Find the ImageView in the layout
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
         val diceImage: ImageView = findViewById(R.id.imageView)
 
         // Determine which drawable resource ID to use based on the dice roll
@@ -44,8 +47,18 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
         // Update the ImageView with the correct drawable resource ID
         diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
 
         // Update the content description
         diceImage.contentDescription = diceRoll.toString()
